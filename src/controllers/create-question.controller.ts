38a364ec029c084b19ee63186a1c0d15c982d7df
Marcos Rene <renemoura789@gmail.com>
2024-12-22 18:59:07 -0300

    refactor: change convertToSlug method

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -41,9 +41,12 @@ export class CreateQuestionController {
   private convertToSlug(title: string): string {
     return title
       .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/[^\w\s-]/g, '')
+      .toLowerCase()
+      .trim()
       .replace(/\s+/g, '-')
+      .replace(/[^\w-]/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
   }
 }
