462b5dd089599a4036da3865316bf768efc7476e
Marcos Rene <renemoura789@gmail.com>
2024-12-24 15:40:49 -0300

    refactor: add authenticate student in the authenticate controller

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 
 import { PrismaService } from './prisma/prima.service'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
@@ -9,11 +10,13 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaStudendsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
     PrismaService,
     { provide: QuestionsRepository, useClass: PrismaQuestionsRepository },
+    { provide: StudentsRepository, useClass: PrismaStudendsRepository },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
@@ -23,6 +26,7 @@ import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question
   exports: [
     PrismaService,
     QuestionsRepository,
+    StudentsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
