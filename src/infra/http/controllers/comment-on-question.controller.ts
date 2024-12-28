d55efaecfe8b4e783dd044126c6ac16358a8459e
Marcos Rene <renemoura789@gmail.com>
2024-12-28 13:21:08 -0300

    feat: add comment on question controller

---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Param, Post } from '@nestjs/common'
+import { z } from 'zod'
+
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnQuestionBodySchema)
+
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private commentOnQuestion: CommentOnQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    await this.commentOnQuestion.execute({
+      content,
+      authorId: userId,
+      questionId,
+    })
+  }
+}
