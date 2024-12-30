b8f972f865f8539e558fb83901a903e8ef1365d5
Marcos Rene <renemoura789@gmail.com>
2024-12-30 11:27:32 -0300

    feat: add upload and create attachment use case

---
+++ src/domain/forum/application/storage/uploader.ts
@@ -0,0 +1,9 @@
+interface UploaderParams {
+  fileName: string
+  fileType: string
+  body: Buffer
+}
+
+export abstract class Uploader {
+  abstract upload(params: UploaderParams): Promise<{ url: string }>
+}
