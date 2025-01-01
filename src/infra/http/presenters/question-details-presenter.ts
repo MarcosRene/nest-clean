4701b93cfaafff0c268cc17ad54f0cd0818213cc
Marcos Rene <renemoura789@gmail.com>
2025-01-01 14:10:59 -0300

    feat: add question details prisma and controllers

---
+++ src/infra/http/presenters/question-details-presenter.ts
@@ -0,0 +1,20 @@
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
+
+import { AttachmentPresenter } from './attachment-presenter'
+
+export class QuestionDetailsPresenter {
+  static toHttp(questionDetails: QuestionDetails) {
+    return {
+      questionId: questionDetails.questionId.toString(),
+      authorId: questionDetails.authorId.toString(),
+      author: questionDetails.author,
+      title: questionDetails.title,
+      slug: questionDetails.slug.value,
+      content: questionDetails.content,
+      attachments: questionDetails.attachments.map(AttachmentPresenter.toHttp),
+      bestAnswerId: questionDetails.bestAnswerId?.toString(),
+      createdAt: questionDetails.createdAt,
+      updatedAt: questionDetails.updatedAt,
+    }
+  }
+}
